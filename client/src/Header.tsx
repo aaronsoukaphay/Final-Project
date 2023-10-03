88af98de4eeadd6a1c9a870c8bf77cee9109d0a8
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-10-03 16:44:03 +0000

    completed feature

---
+++ client/src/Header.tsx
@@ -0,0 +1,92 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { BsCart } from 'react-icons/bs';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import './Header.css';
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const { teamId } = useParams();
+  const [team, setTeam] = useState();
+  const [error, setError] = useState<any>();
+
+  useEffect(() => {
+    async function getTeamInfo() {
+      setError(undefined);
+      try {
+        const response = await fetch(`/api/teams/${teamId}`);
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        const teamInfo = await response.json();
+        setTeam(teamInfo);
+      } catch (err: any) {
+        console.log(err.message);
+        setError(err);
+      }
+    }
+    if (teamId) {
+      getTeamInfo();
+    }
+  }, [teamId]);
+
+  if (error) {
+    console.error('Fetch error:', error);
+    return (
+      <p>Error! {error instanceof Error ? error.message : 'Unknown Error'}</p>
+    );
+  }
+
+  return (
+    <div>
+      <TopBanner />
+      <BottomBanner team={team} />
+      <NavBar team={team} />
+      <Outlet />
+    </div>
+  );
+}
+
+function TopBanner() {
+  return (
+    <div className="row d-flex justify-content-end my-1">
+      <div className="col-2 d-flex justify-content-center">
+        <Button>Sign In</Button>
+      </div>
+      <div className="col-1 d-flex justify-content-start align-items-center">
+        <BsCart />
+      </div>
+    </div>
+  );
+}
+
+function BottomBanner({ team }) {
+  const backgroundColor = team ? team.bannerColor : 'grey';
+  return (
+    <div className="row mt-2 py-4" style={{ backgroundColor: backgroundColor }}>
+      <div className="col-5 d-flex justify-content-center align-items-center">
+        <h4>TOUCHDOWN THREADS</h4>
+      </div>
+      <div className="col"></div>
+      <div className="col d-flex align-items-center">
+        <FaMagnifyingGlass className="mx-3" />
+        <input type="text" name="search" placeholder="Search products..." />
+      </div>
+    </div>
+  );
+}
+
+function NavBar({ team }) {
+  const backgroundColor = team ? team.navColor : 'darkgrey';
+  return (
+    <div className="row" style={{ backgroundColor: backgroundColor }}>
+      <div className="col"></div>
+      <div className="col-5 d-flex justify-content-between py-2">
+        <Link to="/catalog/Jersey">Jerseys</Link>
+        <Link to="/catalog/men">Men</Link>
+        <Link to="/catalog/women">Women</Link>
+      </div>
+      <div className="col"></div>
+    </div>
+  );
+}
---
+++ server/server.ts
@@ -0,0 +1,141 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
+import 'dotenv/config';
+import express from 'express';
+import pg from 'pg';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+// eslint-disable-next-line no-unused-vars -- Remove when used
+const db = new pg.Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+// Create paths for static directories
+const reactStaticDir = new URL('../client/dist', import.meta.url).pathname;
+const uploadsStaticDir = new URL('public', import.meta.url).pathname;
+
+app.use(express.static(reactStaticDir));
+// Static directory for file uploads server/public/
+app.use(express.static(uploadsStaticDir));
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// GETS all teams that have a team logo
+app.get('/api/teams', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+        from "teams"
+        where "teamLogo" != ''
+    `;
+    const result = await db.query(sql);
+    res.json(result.rows);
+  } catch (err: any) {
+    next(err);
+  }
+});
+
+// GETS all products in a specific category
+app.get('/api/products/:category', async (req, res, next) => {
+  try {
+    const category = req.params.category;
+    const sql = `
+      select *
+        from "products"
+        where "category" = $1
+    `;
+    const result = await db.query(sql, [category]);
+    const products = result.rows;
+    if (!products[0]) {
+      throw new ClientError(
+        404,
+        `cannot find products with category: ${category}`
+      );
+    }
+    res.json(products);
+  } catch (err: any) {
+    next(err);
+  }
+});
+
+// GETS all products from one team
+app.get('/api/products/teams/:teamId', async (req, res, next) => {
+  try {
+    const teamId = Number(req.params.teamId);
+    validateTeamId(teamId);
+    const sql = `
+      select *
+        from "products"
+        where "teamId" = $1
+    `;
+    const result = await db.query(sql, [teamId]);
+    const products = result.rows;
+    validateResult(products[0], teamId);
+    res.json(products);
+  } catch (err: any) {
+    next(err);
+  }
+});
+
+// GETS details from one team
+app.get('/api/teams/:teamId', async (req, res, next) => {
+  try {
+    const teamId = Number(req.params.teamId);
+    validateTeamId(teamId);
+    const sql = `
+      select *
+        from "teams"
+        where "teamId" = $1
+    `;
+    const result = await db.query(sql, [teamId]);
+    const teamInfo = result.rows[0];
+    validateResult(teamInfo, teamId);
+    res.json(teamInfo);
+  } catch (err: any) {
+    next(err);
+  }
+});
+
+function validateTeamId(teamId: number) {
+  if (!Number.isInteger(teamId) || teamId <= 0) {
+    throw new ClientError(400, 'teamId must be a positive integer');
+  }
+}
+
+function validateResult(result: object, teamId: number) {
+  if (!result) {
+    throw new ClientError(
+      404,
+      `cannot find products for team with teamId: ${teamId}`
+    );
+  }
+}
+
+/**
+ * Serves React's index.html if no api route matches.
+ *
+ * Implementation note:
+ * When the final project is deployed, this Express server becomes responsible
+ * for serving the React files. (In development, the Vite server does this.)
+ * When navigating in the client, if the user refreshes the page, the browser will send
+ * the URL to this Express server instead of to React Router.
+ * Catching everything that doesn't match a route and serving index.html allows
+ * React Router to manage the routing.
+ */
+app.get('*', (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
+});
