cb61b9fac4614682f7e253308b8b56c2afccbc7d
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-10-04 19:21:30 +0000

    finished product details layout

--- client/src/Cart.tsx
+++ client/src/Cart.tsx
@@ -1,13 +1,47 @@
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+
 export default function Cart() {
   return (
     <div className="p-5 d-flex">
       <div className="col-7 me-auto">
         <h2>Order Details</h2>
         <div>Your Order will be with you soon</div>
-        <div className="d-flex mt-4 border-bottom">
-          <div className="col-4 me-auto">Product</div>
-          <div className="col-1 me-auto">Quantity</div>
-          <div className="col-2 d-flex justify-content-end">Subtotal</div>
+        <div className="d-flex mt-4">
+          <div className="col-3 d-flex flex-column">
+            <div className="text-center border-bottom border-2 border-dark">
+              Product
+            </div>
+            <div className="text-center mt-2">
+              <img src="/images/stefon-diggs.webp" width="60%" />
+            </div>
+          </div>
+          <div className="d-flex flex-column">
+            <div className="border-bottom border-2 border-dark">
+              Description
+            </div>
+            <div className="d-flex flex-column mt-2">
+              <div>Stefon Diggs Buffalo Bills Football Jersey</div>
+              <div>Size: S</div>
+            </div>
+          </div>
+          <div className="col-2 d-flex flex-column">
+            <div className="border-bottom border-2 border-dark">Quantity</div>
+            <div className="mt-2">
+              <DropdownButton id="dropdown-basic-button" title="1">
+                <Dropdown.Item href="#/quantity-1">1</Dropdown.Item>
+                <Dropdown.Item href="#/quantity-2">2</Dropdown.Item>
+                <Dropdown.Item href="#/quantity-3">3</Dropdown.Item>
+                <Dropdown.Item href="#/quantity-4">4</Dropdown.Item>
+                <Dropdown.Item href="#/quantity-5">5</Dropdown.Item>
+              </DropdownButton>
+            </div>
+          </div>
+          <div className="col-2 d-flex flex-column">
+            <div className="border-bottom border-2 border-dark">Subtotal</div>
+            <div className="mt-2">
+              <div>$130.00</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="col-4">
